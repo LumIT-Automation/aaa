0dca2d5f023c300852d4235c73fa7c62a43418ed
mik <m.sartori@lumit.it>
2022-04-14 15:45:29 +0200

    sso-reset-admin-password.sh ask input

--- CONTAINER-DEBIAN-PKG/usr/bin/sso-reset-admin-password.sh
+++ CONTAINER-DEBIAN-PKG/usr/bin/sso-reset-admin-password.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <admin_password>"
-    echo "Script to reset the admin password of the sso service."
+read -rsp "Enter a new password for admin@automation.local: " passwd1
+echo
+read -rsp "Confirm the new password for admin@automation.local: " passwd2
+echo
+
+if [ "$passwd1" != "$passwd2" ]; then
+    echo -e "\tEntered passwords differ."
+    echo -e "\tPassword not changed"
     exit 1
-else
-    adminPwd="$1"
 fi
+
 
 podman exec sso bash -c "cd /var/www/aaa; \
 mv /var/lib/aaa-venv/bin/manage.py .; \
 source /var/lib/aaa-venv/bin/activate; \
-echo \"from django.contrib.auth.models import User; usr = User.objects.get(username='admin@automation.local'); usr.set_password('$adminPwd'); usr.save()\" | python manage.py shell; \
+echo \"from django.contrib.auth.models import User; usr = User.objects.get(username='admin@automation.local'); usr.set_password('$passwd1'); usr.save()\" | python manage.py shell; \
 deactivate; \
 mv manage.py /var/lib/aaa-venv/bin;"
 
+if [ $? -eq 0 ]; then
+    echo "Password changed"
+fi
+
 exit 0
+
